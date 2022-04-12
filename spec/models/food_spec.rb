9e8b07b80aa38c687a3d82238a06eb18f13bf869
tutdeputraw <anoboy.code@gmail.com>
2022-04-12 14:53:27 +0000

    Add validation for Food, which only involves all foods that contain the 'N' letter in its name

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -40,4 +40,28 @@ RSpec.describe Food, type: :model do
     
     expect(food2.errors[:name]).to include("has already been taken")
   end
+
+  describe 'self#by_letter' do
+    it "should return a sorted array of results that match" do
+      food1 = Food.create(
+        name: "Nasi Uduk",
+        description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
+        price: 10000.0
+      )
+
+      food2 = Food.create(
+        name: "Kerak Telor",
+        description: "Betawi traditional spicy omelette made from glutinous rice cooked with egg and served with serundeng.",
+        price: 8000.0
+      )
+
+      food3 = Food.create(
+        name: "Nasi Semur Jengkol",
+        description: "Based on dongfruit, this menu promises a unique and delicious taste with a small hint of bitterness.",
+        price: 8000.0
+      )
+
+      expect(Food.by_letter("N")).to eq([food3, food1])
+    end
+  end
 end
